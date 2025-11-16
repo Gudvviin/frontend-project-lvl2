1c65f344b82d550cc4ce0ffc806b61ebb9a748d3
Afanasev Mikhail <miki123451@mail.ru>
2025-11-17 00:22:25 +0300

    1

---
+++ gendiff.js
@@ -0,0 +1,11 @@
+#!/Projects/project2 node
+import genDiff from '@hexlet/code';
+    
+    const { program } = require('commander');
+
+    program
+    .name('gendiff')
+    .option('-h')
+  const diff = genDiff(filepath1, filepath2);
+  console.log(diff);
+  console.log('qwe');
