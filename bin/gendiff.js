f1c38af48ab921367ab24d109b3378dea94220c1
Afanasev Mikhail <miki123451@mail.ru>
2025-11-27 22:49:53 +0300

    add two task

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,8 @@ import { program } from 'commander';
 
 program
     .version('0.0.1')
+    .option('-f, --format [type]',  'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
     program.parse(process.argv);
