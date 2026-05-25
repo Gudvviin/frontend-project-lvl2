ab534ec6223459e9c2def900ef1b6bae30494b14
Afanasev <miki123451@mail.ru>
2026-05-25 15:12:13 +0300

    f.functions.js, deleted function getArr..

--- src/functions.js
+++ src/functions.js
@@ -4,26 +4,17 @@ import _ from "lodash";
 
 const {forEach} = pkg;
 
-  function getArrOfObjects (object) {
-    const arr = [];
-    Object.keys(object).forEach((key) => {
-      arr.push({keyName: key, keyValue: object[key]})
-    })
-    return arr;
-  };
-
   function convertsToArray (obj1, obj2){
-    console.log(Object.keys(obj1).map(item => ({keyName: item , keyValue: obj1[item] })));
-    const result = _.unionBy(getArrOfObjects(obj1), getArrOfObjects(obj2), "keyName")
+    const result = _.unionBy(Object.keys(obj1), Object.keys(obj2))
     .map((item) => { const isKeyTrue = (item, obj) => obj.hasOwnProperty(item); 
-      if (isKeyTrue(item.keyName, obj1) === isKeyTrue(item.keyName, obj2) && item.keyValue === obj1[item.keyName] && item.keyValue === obj2[item.keyName]){
-        return {...item, keyStatus: "hasn't changed"};
-      } else if (isKeyTrue(item.keyName, obj1) === isKeyTrue(item.keyName, obj2) && item.keyValue !== obj1[item.keyValue]) {
-        return {...item, keyValue2: obj2[item.keyName], keyStatus: "has changed"}
-      } else  if (isKeyTrue(item.keyName, obj1)){
-        return {...item, keyStatus: "deleted"}
-      } else  if (isKeyTrue(item.keyName, obj2)){
-        return {...item, keyStatus: "add"}
+      if (isKeyTrue(item, obj1) === isKeyTrue(item, obj2) && obj1[item] === obj2[item]){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "hasn't changed"};
+      } else if (isKeyTrue(item, obj1) === isKeyTrue(item, obj2) && obj1[item] !== obj2[item]) {
+        return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "has changed"}
+      } else  if (isKeyTrue(item, obj1)){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "deleted"}
+      } else  if (isKeyTrue(item, obj2)){
+        return {keyName: item, keyValue: obj2[item], keyStatus: "add"}
       } 
      return 
     }); 
