5ee6081f1e795a054465a733c8d09463a2f7a8c4
Afanasev <miki123451@mail.ru>
2026-05-25 15:25:46 +0300

    f.functions.js, deleted const isKey...

--- src/functions.js
+++ src/functions.js
@@ -6,15 +6,15 @@ const {forEach} = pkg;
 
   function convertsToArray (obj1, obj2){
     const result = _.unionBy(Object.keys(obj1), Object.keys(obj2))
-    .map((item) => { const isKeyTrue = (item, obj) => obj.hasOwnProperty(item); 
-      if (isKeyTrue(item, obj1) === isKeyTrue(item, obj2) && obj1[item] === obj2[item]){
-        return {keyName: item, keyValue: obj1[item], keyStatus: "hasn't changed"};
-      } else if (isKeyTrue(item, obj1) === isKeyTrue(item, obj2) && obj1[item] !== obj2[item]) {
-        return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "has changed"}
-      } else  if (isKeyTrue(item, obj1)){
-        return {keyName: item, keyValue: obj1[item], keyStatus: "deleted"}
-      } else  if (isKeyTrue(item, obj2)){
-        return {keyName: item, keyValue: obj2[item], keyStatus: "add"}
+    .map((item) => { 
+      if (_.has(obj1, item) === _.has(obj2, item) && obj1[item] === obj2[item]){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "unchanged"};
+      } else if (_.has(obj1, item) === _.has(obj2, item) && obj1[item] !== obj2[item]) {
+        return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "changed"}
+      } else  if (_.has(obj1, item)){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "removed"}
+      } else  if (_.has(obj2, item)){
+        return {keyName: item, keyValue: obj2[item], keyStatus: "added"}
       } 
      return 
     }); 
