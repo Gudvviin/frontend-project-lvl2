4e0dac1aa862775d142a1b5530219ce693c250e2
Afanasev <miki123451@mail.ru>
2026-05-26 15:28:01 +0300

    f.functions.js, correction if Guard Expression

--- src/functions.js
+++ src/functions.js
@@ -3,19 +3,20 @@ import _ from "lodash";
 
   function convertsToArray (obj1, obj2){
     const result = _.unionBy(Object.keys(obj1), Object.keys(obj2))
-    .map((item) => { 
-      if (_.has(obj1, item) === _.has(obj2, item) && obj1[item] === obj2[item]){
-        return {keyName: item, keyValue: obj1[item], keyStatus: "unchanged"};
-      }
-      if (_.has(obj1, item) === _.has(obj2, item) && obj1[item] !== obj2[item]) {
-        return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "changed"}
-      }
-      if (_.has(obj1, item)){
-        return {keyName: item, keyValue: obj1[item], keyStatus: "removed"}
-      } 
-      if (_.has(obj2, item)){
+    .map((item) => {
+      if (!_.has(obj1, item)){
         return {keyName: item, keyValue: obj2[item], keyStatus: "added"}
       } 
+      if (!_.has(obj2, item)){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "removed"}
+      } 
+      if (obj1[item] === obj2[item]){
+        return {keyName: item, keyValue: obj1[item], keyStatus: "unchanged"};
+      }
+      if (obj1[item] !== obj2[item]) {
+        return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "changed"}
+      }
+    
     }); 
     return result;
 };
