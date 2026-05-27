f4f456d416a7c0210bfad8226e84e82166f3d4a6
Afanasev <miki123451@mail.ru>
2026-05-27 15:10:31 +0300

    df.test, correction test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -16,14 +16,14 @@ test('should correctly compare two objects', () => {
 };
 
     const expected = {
-  ' - follow': false,
-  ' host': 'hexlet.io',
-  ' - proxy': '123.234.53.22',
-  ' - timeout': 50,
-  ' + timeout': 20,
-  ' + verbose': true
+  "- follow": false,
+  "host": "hexlet.io",
+  "- proxy": "123.234.53.22",
+  "- timeout": 50,
+  "+ timeout": 20,
+  "+ verbose": true
 };
     const result = convertsToArray(obj1, obj2)
-    expect(result).toBe(expected)
+    expect(result).toStrictEqual(expected)
 });
 });
---
+++ __tests__/parsers.test.js
@@ -0,0 +1,16 @@
+// @ts-check
+import {isObject} from '../src/parsers.js';
+
+describe('mergeSorting function', () => {
+test('should compare two files correctly yml', () => {
+  const nameFile = "filepath1.json";
+    const expected = {
+  "host": "hexlet.io",
+  "timeout": 50,
+  "proxy": "123.234.53.22",
+  "follow": false
+};
+    const result = isObject(nameFile)
+    expect(result).toEqual(expected)
+});
+});
