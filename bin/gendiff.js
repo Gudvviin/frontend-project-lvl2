27f883a2d8afe273d2f20278e94d794402fe1c9b
Afanasev <miki123451@mail.ru>
2026-05-27 14:15:32 +0300

    f.function.js, added sorting

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { convertsToArray, formattingForOutput } from '../src/functions.js';
+import { convertsToArray } from '../src/functions.js';
 import { isObject } from '../src/parsers.js'
 
 program
@@ -10,7 +10,7 @@ program
     .option('-f, --format [type]',  'output format')    // опеределение флагов ком.стр.,которые помогают с запуском
     .arguments('<args...>')
     .action((args) => {
-        console.log(formattingForOutput(convertsToArray(isObject(args[0]), isObject(args[1]))));
+        console.log(convertsToArray(isObject(args[0]), isObject(args[1])));
     });
     
 program.parse();
--- src/functions.js
+++ src/functions.js
@@ -18,27 +18,62 @@ import _ from "lodash";
       }
     
     }); 
-    return result;
+   
+    function subsequence (obj){
+    const objectLength = Object.keys(obj).length;
+    const objectResult = [];
+    for (let i = 0; i < objectLength / 2; i++){ 
+      objectResult.push(Object.keys(obj)[objectLength - 1 - i]);
+      objectResult.push(Object.keys(obj)[i]);
+      
+    }
+    return objectResult;
+  }
+    const callArr = [...new Set([...subsequence(obj1), ...subsequence(obj2)])];
+   function listTransformation(list, arr) {
+    const obj = {};
+    list.forEach(element => {
+      const result = arr.find(item => item.keyName === element);
+      if (result.keyStatus === "removed"){
+        obj["-"+result.keyName] = result.keyValue;
+      }
+      if (result.keyStatus === "unchanged") {
+        obj[""+result.keyName] = result.keyValue;
+      }
+      if (result.keyStatus === "changed") {
+        obj["-"+result.keyName] = result.keyValue;
+        obj["+"+result.keyName] = result.keyValue2;
+      }
+      if (result.keyStatus === "added") {
+        obj["+"+result.keyName] = result.keyValue;
+      }
+        
+    });
+    return obj
+   }
+   
+    return listTransformation(callArr, result);
 };
-function formattingForOutput (data){
-const result = data
-.reduce((acc, item) => {
-  if (item.keyStatus === "removed"){
-    acc["-"+item.keyName] = item.keyValue;
-  }
-  if (item.keyStatus === "unchanged"){
-    acc[" "+item.keyName] = item.keyValue;
-  }
-  if (item.keyStatus === "changed"){
-    acc["-"+item.keyName] = item.keyValue;
-    acc["+"+item.keyName] = item.keyValue2;
-  }
-  if (item.keyStatus === "added"){
-    acc["+"+item.keyName] = item.keyValue;
-  }
-  return acc
-}, {})
-return result
-}
+// function formattingForOutput (data){
 
-  export {convertsToArray,formattingForOutput};
+// const result = data
+// .reduce((acc, item) => {
+//   if (item.keyStatus === "removed"){
+//     acc["-"+item.keyName] = item.keyValue 
+//   }
+//    if (item.keyStatus === "unchanged"){
+//     acc[""+item.keyName] = item.keyValue;
+//   }
+//   if (item.keyStatus === "changed"){
+//     acc["-"+item.keyName] = item.keyValue;
+//     acc["+"+item.keyName] = item.keyValue2;
+//   }
+//    if (item.keyStatus === "added"){
+//     acc["+"+item.keyName] = item.keyValue;
+//   }
+//   return acc
+// }, {})
+// return result
+// }
+
+  export {convertsToArray};
