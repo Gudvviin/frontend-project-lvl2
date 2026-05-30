2ce68be341886e4a44e6e150cb57edbb50a9e938
Afanasev <miki123451@mail.ru>
2026-05-30 19:58:25 +0300

    f.functions.js, error recursion

--- src/functions.js
+++ src/functions.js
@@ -3,76 +3,63 @@ import _ from "lodash";
   function convertsToArray (obj1, obj2){
     const result = _.unionBy(Object.keys(obj1), Object.keys(obj2))
     .map((item) => {
+      if (typeof obj1[item] === "object" && typeof obj2[item] === "object" && obj1[item] !== null &&  obj2[item] !== null)
+        {
+          return {keyName: item, keyValue: convertsToArray(obj1[item], obj2[item])};
+        } else {
+          
       if (!_.has(obj1, item)){
         return {keyName: item, keyValue: obj2[item], keyStatus: "added"}
-      } 
+      }
       if (!_.has(obj2, item)){
         return {keyName: item, keyValue: obj1[item], keyStatus: "removed"}
-      } 
+      }
       if (obj1[item] === obj2[item]){
         return {keyName: item, keyValue: obj1[item], keyStatus: "unchanged"};
       }
       if (obj1[item] !== obj2[item]) {
         return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "changed"}
       }
-    
-    }); 
-   
-    function subsequence (obj){
-    const objectLength = Object.keys(obj).length;
-    const objectResult = [];
-    for (let i = 0; i < objectLength / 2; i++){ 
-      objectResult.push(Object.keys(obj)[objectLength - 1 - i]);
-      objectResult.push(Object.keys(obj)[i]);
+     
+        }
+  }); 
+  console.log(result)
+  return result
+  //   function subsequence (obj){
+  //   const objectLength = Object.keys(obj).length;
+  //   const objectResult = [];
+  //   for (let i = 0; i < objectLength / 2; i++){ 
+  //     objectResult.push(Object.keys(obj)[objectLength - 1 - i]);
+  //     objectResult.push(Object.keys(obj)[i]);
       
-    }
-    return objectResult;
-  }
-    const callArr = [...new Set([...subsequence(obj1), ...subsequence(obj2)])];
-   function listTransformation(list, arr) {
-    const arrayStrings = list.map((element) => {
-      const objElement = arr.find(item => item.keyName === element);
-      if (objElement.keyStatus === "removed"){
-        return `- ${objElement.keyName}: ${objElement.keyValue}`
-      }
-      if (objElement.keyStatus === "unchanged"){
-        return `  ${objElement.keyName}: ${objElement.keyValue}`
-      }
-      if (objElement.keyStatus === "changed"){
-        return [
-          `- ${objElement.keyName}: ${objElement.keyValue}`, 
-          `+ ${objElement.keyName}: ${objElement.keyValue2}`
-        ].join('\n')
-      }
-      if (objElement.keyStatus === "added") {
-        return `+ ${objElement.keyName}: ${objElement.keyValue}`
-      }
-    });
-    return `{\n${arrayStrings.join('\n')}\n}`
-   };
+  //   }
+  //   return objectResult;
+  // }
+  //   const callArr = [...new Set([...subsequence(obj1), ...subsequence(obj2)])];
+  //  function listTransformation(list, arr) {
+  //   const arrayStrings = list.map((element) => {
+  //     const objElement = arr.find(item => item.keyName === element);
+  //     if (objElement.keyStatus === "removed"){
+  //       return `- ${objElement.keyName}: ${objElement.keyValue}`
+  //     }
+  //     if (objElement.keyStatus === "unchanged"){
+  //       return `  ${objElement.keyName}: ${objElement.keyValue}`
+  //     }
+  //     if (objElement.keyStatus === "changed"){
+  //       return [
+  //         `- ${objElement.keyName}: ${objElement.keyValue}`, 
+  //         `+ ${objElement.keyName}: ${objElement.keyValue2}`
+  //       ].join('\n')
+  //     }
+  //     if (objElement.keyStatus === "added") {
+  //       return `+ ${objElement.keyName}: ${objElement.keyValue}`
+  //     }
+  //   });
+  //   return `{\n${arrayStrings.join('\n')}\n}`
+  //  };
    
-    return listTransformation(callArr, result);
+    // return listTransformation(callArr, result);
 };
-// function formattingForOutput (data){
 
-// const result = data
-// .reduce((acc, item) => {
-//   if (item.keyStatus === "removed"){
-//     acc["-"+item.keyName] = item.keyValue 
-//   }
-//    if (item.keyStatus === "unchanged"){
-//     acc[""+item.keyName] = item.keyValue;
-//   }
-//   if (item.keyStatus === "changed"){
-//     acc["-"+item.keyName] = item.keyValue;
-//     acc["+"+item.keyName] = item.keyValue2;
-//   }
-//    if (item.keyStatus === "added"){
-//     acc["+"+item.keyName] = item.keyValue;
-//   }
-//   return acc
-// }, {})
-// return result
-// }
 
   export {convertsToArray};
