1a94cbef113e4058b20cf0928d3918c33319d9cc
Afanasev <miki123451@mail.ru>
2026-06-03 20:34:13 +0300

    f.fuctions.js. error

--- src/functions.js
+++ src/functions.js
@@ -3,57 +3,58 @@ import _ from "lodash";
   function convertsToArray (obj1, obj2){
     const result = _.unionBy(Object.keys(obj1), Object.keys(obj2))
     .map((item) => {
-      // console.log(typeof obj1[item], typeof obj2[item])
-      // if (typeof obj2[item] === "object"  && obj2[item] !== null && typeof obj2[item] === undefined) {
-      //   console.log(item,'1')
-      //   return {keyName: item, keyValue: obj2[item].keyValue, keyStatus: "object1"}
-      // }
-      // console.log(typeof obj1[item] === "object" , obj1[item] !== null , typeof obj2[item] === "object"  , obj2[item] !== null, " ||",
-      //   typeof obj1[item] === "object" && obj1[item] !== null && typeof obj2[item] === "object"  && obj2[item] !== null,
-      //    item,"осн") 
- if (typeof obj2[item] === "object"  && obj2[item] !== null) {
-return {keyName: item, keyValue: convertsToArray(obj2[item], obj2[item]), keyStatus: "object"}
-    } else if (typeof obj1[item] === "object" && obj1[item] !== null ) {
-      return {keyName: item, keyValue: convertsToArray(obj1[item], obj1[item]), keyStatus: "object"}
-    } else if (typeof obj1[item] === "object" && obj1[item] !== null && typeof obj2[item] === "object"  && obj2[item] !== null ) {
-      return {keyName: item, keyValue: convertsToArray(obj1[item], obj2[item]), keyStatus: "object"}
-    }
-     else {
+       if (typeof obj1[item] === "object" && typeof obj2[item] === "object" && obj1[item] !== null && obj2[item] !== null) {
+        return {keyName: item, keyValue: convertsToArray(obj1[item], obj2[item]), keyStatus: "object"};
+      } 
+       else if (typeof obj1[item] === "object" && obj1[item] !== null) {
+        return {keyName: item, keyValue: convertsToArray(obj1[item], obj1[item]), keyStatus: "object"};
+      }
+        else if (typeof obj2[item] === "object" && obj2[item] !== null){
+        return {keyName: item, keyValue: convertsToArray(obj2[item], obj2[item]), keyStatus: "object"};
+      } 
+      else {
       if (!_.has(obj1, item)){
         return {keyName: item, keyValue: obj2[item], keyStatus: "added"}
       } 
       if (!_.has(obj2, item)){
         return {keyName: item, keyValue: obj1[item], keyStatus: "removed"}
-      }
+      } 
       if (obj1[item] === obj2[item]){
         return {keyName: item, keyValue: obj1[item], keyStatus: "unchanged"};
-      }
+      } 
       if (obj1[item] !== obj2[item]) {
         return {keyName: item, keyValue: obj1[item], keyValue2: obj2[item], keyStatus: "changed"}
       }
-       
     }
+    
   }); 
- 
-  return result;
-  //   function subsequence (obj){
-  //   const objectLength = Object.keys(obj).length;
-  //   const objectResult = [];
-  //   for (let i = 0; i < objectLength / 2; i++){ 
-  //     objectResult.push(Object.keys(obj)[objectLength - 1 - i]);
-  //     objectResult.push(Object.keys(obj)[i]);
-      
-  //   }
-  //   return objectResult;
-  // }
+   
+
+    function subsequence (obj){
+
+    const objectLength = Object.keys(obj).length;
+    const objectResult = [];
+
+    for (let i = 0; i < objectLength / 2; i++){ 
+
+      objectResult.push(Object.keys(obj)[objectLength - 1 - i]);
+      objectResult.push(Object.keys(obj)[i]);
+    
+    }
+    
+    return objectResult;
+  }
+  return subsequence(obj1)
+
   //   const callArr = [...new Set([...subsequence(obj1), ...subsequence(obj2)])];
   //  function listTransformation(list, arr) {
   //   const arrayStrings = list.map((element) => {
   //     const objElement = arr.find(item => item.keyName === element);
+  //     console.log(list)
   //     if (objElement.keyStatus === "removed"){
   //       return `- ${objElement.keyName}: ${objElement.keyValue}`
   //     }
-  //     if (objElement.keyStatus === "unchanged"){
+  //     if (objElement.keyStatus === "unchanged" || objElement.keyStatus === "object"){
   //       return `  ${objElement.keyName}: ${objElement.keyValue}`
   //     }
   //     if (objElement.keyStatus === "changed"){
@@ -69,7 +70,7 @@ return {keyName: item, keyValue: convertsToArray(obj2[item], obj2[item]), keySta
   //   return `{\n${arrayStrings.join('\n')}\n}`
   //  };
    
-    // return listTransformation(callArr, result);
+  //   return listTransformation(callArr, result);
 };
 
 
