a35bd6f3bf5dc296ccc2ae53b88d279e0f9c0a63
Afanasev <miki123451@mail.ru>
2026-06-05 15:04:16 +0300

    f. gendiff.js Add the current formatter as the default formatter for the library

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,26 @@ program
     .version('0.0.1', '-v, --version', 'output the version number')
     .description('Compares two configuration files and shows a difference.')    //описывает программу
     .option('-f, --format [type]', 'output format')    // опеределение флагов ком.стр.,которые помогают с запуском
+    .option('-s, --stylish <type>', 'default formatter for', listTransformation)
+    .option('-a, --atr [type]', listTransformation)
     .arguments('<args...>')
     .action((args) => {
-        console.log(stylish(convertsToArray(isObject(args[0]), isObject(args[1])), 1, "-"));
+        const options = program.opts();
+        const diff = convertsToArray(isObject(args[0]), isObject(args[1]));
+        console.log(listTransformation(diff))
+
     });
 
+program.command('diff')
+    .description('Compares two configuration files and shows the differences by defining indents')
+    .option('-f, --format [type]', 'output format')    // опеределение флагов ком.стр.,которые помогают с запуском
+    .option('-s, --stylish <type>', 'default formatter for', stylish)
+    .option('-a, --atr [type]', listTransformation)
+    .arguments('<args...>')
+    .action((args) => {
+        const options = program.opts();
+        const diff = convertsToArray(isObject(args[0]), isObject(args[1]));
+        console.log(stylish(diff, 0, " "))
+
+    });
 program.parse();
--- commander/stylish.js
+++ commander/stylish.js
@@ -2,27 +2,27 @@ function stylish(data, int = 0, s = " ") {
     const sign = s;
     const result = data.map((item) => {
         if (item.keyStatus === "object") {
-            return `${sign.repeat(int + 1)}  ${item.keyName}:${stylish(item.keyValue, int+1, sign)}`
-        } else if (item.keyStatus === "object, removed"){
-            return `${sign.repeat(int + 1)}- ${item.keyName}:${stylish(item.keyValue, int+1, sign)}`
+            return `${sign.repeat(int + 1)}  ${item.keyName}:${stylish(item.keyValue, int + 1, sign)}`
+        } else if (item.keyStatus === "object, removed") {
+            return `${sign.repeat(int + 1)}- ${item.keyName}:${stylish(item.keyValue, int + 1, sign)}`
         } else if (item.keyStatus === "object, add") {
-            return `${sign.repeat(int + 1)}+ ${item.keyName}:${stylish(item.keyValue, int+1, sign)}`
+            return `${sign.repeat(int + 1)}+ ${item.keyName}:${stylish(item.keyValue, int + 1, sign)}`
         } else {
             if (item.keyStatus === "removed" || item.keyStatus === "object, removed") {
-                return `${sign.repeat(int + 2)}- ${item.keyName}: ${item.keyValue} # Удалено`
+                return `${sign.repeat(int + 2)}- ${item.keyName}: ${item.keyValue}`
             }
             if (item.keyStatus === "unchanged") {
-                return `${sign.repeat(int + 2)} ${item.keyName}: ${item.keyValue} `
+                return `${sign.repeat(int + 2)} ${item.keyName}: ${item.keyValue}`
             }
             if (item.keyStatus === "changed") {
                 return [
-                    `${sign.repeat(int + 2)}- ${item.keyName}: ${item.keyValue} # Строе значение`,
-                    `${sign.repeat(int + 2)}+ ${item.keyName}: ${item.keyValue2} # Новое значение`
+                    `${sign.repeat(int + 2)}- ${item.keyName}: ${item.keyValue}`,
+                    `${sign.repeat(int + 2)}+ ${item.keyName}: ${item.keyValue2}`
                 ].join('\n')
 
             }
             if (item.keyStatus === "added" || item.keyStatus === "object, add") {
-                return `${sign.repeat(int + 2)}+ ${item.keyName}: ${item.keyValue} # Добавлено`
+                return `${sign.repeat(int + 2)}+ ${item.keyName}: ${item.keyValue} `
             }
         }
 
