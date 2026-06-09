0ec66d7e05bd4fac282b6ef05635fdd4855d644c
Afanasev <miki123451@mail.ru>
2026-06-09 22:06:39 +0300

    f.plain.js cor function plain

--- commander/plain.js
+++ commander/plain.js
@@ -1,39 +1,46 @@
-function plain(arr, acc = []) {
-
-  
+function plain(arr, acc) {
+  const arrName = [];
+  if (typeof acc !== 'undefined') {
+    arrName.push(acc)
+  }
   const arrayStrings = arr.map((element) => {
-
     if (element.keyStatus === "object") {
-      acc.push(element.keyName)
-      return `${plain(element.keyValue, acc)}`
+      const clonArr = [...arrName];
+      clonArr.push(element.keyName)
+      return `${plain(element.keyValue, clonArr.join('.'))}`
     } else if (element.keyStatus === "object, removed") {
-      if (typeof acc === "undefined") {
+      if (typeof arrName === "undefined") {
         return `Property '${element.keyName}' was removed`
       } else {
-        return `Property '${acc.join('.')}.${element.keyName}' was removed`
+        if (arrName.length !== 0) {
+          return `Property '${arrName.join('.')}.${element.keyName}' was removed`
+        } else {
+          return `Property '${element.keyName}' was removed`
+        }
       }
     }
     else if (element.keyStatus === "object, add") {
-      if (typeof acc === "undefined") {
+      if (typeof arrName === "undefined") {
         return `Property '${element.keyName}' was added with value: [complex value]`
       } else {
-        return `Property '${acc.join('.')}.${element.keyName}' was added with value: [complex value]`
+        if (arrName.length !== 0) {
+          return `Property '${arrName.join('.')}.${element.keyName}' was added with value: [complex value]`
+        } else {
+          return `Property '${element.keyName}' was added with value: [complex value]`
+        }
       }
-      
     } else {
       if (element.keyStatus === "removed") {
-        return `Property '${acc.join('.')}.${element.keyName}' was removed`
+        return `Property '${arrName.join('.')}.${element.keyName}' was removed`
       }
       if (element.keyStatus === "unchanged") {
-
-        return `Property '${acc.join('.')}.${element.keyName}' was added with value: [complex value]`
+        return `Property '${arrName.join('.')}.${element.keyName}' was added with value: [complex value]`
       }
       if (element.keyStatus === "changed") {
-        return `Property '${acc.join('.')}.${element.keyName}' was updated. From '${element.keyValue}' tot '${element.keyValue2}'`
+        return `Property '${arrName.join('.')}.${element.keyName}' was updated. From '${element.keyValue}' tot '${element.keyValue2}'`
       }
       if (element.keyStatus === "added") {
-          return `Property '${acc.join('.')}.${element.keyName}' was added with value: ${element.keyValue}`
-
+        return `Property '${arrName.join('.')}.${element.keyName}' was added with value: ${element.keyValue}`
       }
     }
   });
